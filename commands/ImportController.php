70fdaf6e0a05cf70998a16841c733b66c18900b1
Tobias Hößl <tobias@hoessl.eu>
2015-12-30 18:33:40 +0100

    Import

--- commands/ImportController.php
+++ commands/ImportController.php
@@ -4,13 +4,98 @@ namespace app\commands;
 
 use app\models\Antrag;
 use app\models\Stadtraetin;
-use app\models\Tag;
 use yii\console\Controller;
 
 class ImportController extends Controller
 {
 
-    public $defaultAction = 'import-from-file';
+    public $defaultAction = 'import-from-mt';
+
+    /**
+     * @param array $antragData
+     */
+    private function importDatarow($antragData)
+    {
+        /** @var Antrag $antrag */
+        $antrag = Antrag::findOne(['ris_id' => $antragData['id']]);
+        if ( ! $antrag) {
+            $antrag         = new Antrag();
+            $antrag->ris_id = $antragData['id'];
+        } else {
+            if ($antrag->status != $antragData['status']) {
+                $antrag->notiz = trim($antrag->notiz) . "\n";
+                $antrag->notiz .= date("d.m.Y.") . ": Status: " . $antrag->status . " -> " . $antragData['status'];
+            }
+            if ($antrag->bearbeitungsfrist != $antragData['bearbeitungsfrist']) {
+                $antrag->notiz = trim($antrag->notiz) . "\n";
+                $antrag->notiz .= date("d.m.Y.") . ": Bearbeitungsfrist: " .
+                                  $antrag->bearbeitungsfrist . " -> " . $antragData['bearbeitungsfrist'];
+                $antrag->bearbeitungsfrist_benachrichtigung = null;
+            }
+            if ($antrag->fristverlaengerung != $antragData['fristverlaengerung']) {
+                $antrag->notiz = trim($antrag->notiz) . "\n";
+                $antrag->notiz .= date("d.m.Y.") . ": Fristverlängerung: " .
+                                  $antrag->fristverlaengerung . " -> " . $antragData['fristverlaengerung'];
+                $antrag->fristverlaengerung_benachrichtigung = null;
+            }
+            if ($antrag->gestellt_am != $antragData['gestellt_am']) {
+                $antrag->notiz = trim($antrag->notiz) . "\n";
+                $antrag->notiz .= date("d.m.Y.") . ": Gestellt am: " .
+                                  $antrag->gestellt_am . " -> " . $antragData['gestellt_am'];
+            }
+        }
+        $antrag->titel              = mb_substr($antragData['betreff'], 0, 200);
+        $antrag->typ                = $antragData['typ'];
+        $antrag->antrags_nr         = $antragData['antrags_nr'];
+        $antrag->gestellt_am        = $antragData['gestellt_am'];
+        $antrag->bearbeitungsfrist  = $antragData['bearbeitungsfrist'];
+        $antrag->fristverlaengerung = $antragData['fristverlaengerung'];
+        $antrag->status             = $antragData['status'];
+        $antrag->notiz              = '';
+        $antrag->save();
+
+        foreach ($antrag->stadtraetinnen as $stadtraetin) {
+            $antrag->unlink('stadtraetinnen', $stadtraetin, true);
+        }
+
+        foreach ($antragData['stadtraetInnen'] as $stadtraetInData) {
+            $stadtraetin = Stadtraetin::findOne(['ris_id' => $stadtraetInData['id']]);
+            if ( ! $stadtraetin) {
+                $stadtraetin         = new Stadtraetin();
+                $stadtraetin->ris_id = $stadtraetInData['id'];
+                $stadtraetin->name   = $stadtraetInData['name'];
+                $stadtraetin->save();
+            }
+            $antrag->link('stadtraetinnen', $stadtraetin);
+        }
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     * @throws \Exception
+     */
+    private function downloadUrl($url)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Stadtrats-Antrags-Tool');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $text = curl_exec($ch);
+        if ($text === false) {
+            throw new \Exception("Import fehlgeschlagen: keine Daten von MT erhalten.");
+        }
+        curl_close($ch);
+
+        return $text;
+    }
 
     /**
      * Daten aus einer JSON-Datei importieren
@@ -22,52 +107,25 @@ class ImportController extends Controller
         $data = file_get_contents($filename);
         $data = json_decode($data, true);
         foreach ($data as $antragData) {
-            /** @var Antrag $antrag */
-            $antrag = Antrag::findOne(['ris_id' => $antragData['id']]);
-            if ( ! $antrag) {
-                $antrag         = new Antrag();
-                $antrag->ris_id = $antragData['id'];
-            } else {
-                if ($antrag->status != $antragData['status']) {
-                    $antrag->notiz = trim($antrag->notiz) . "\n";
-                    $antrag->notiz .= date("d.m.Y.") . ": Status: " . $antrag->status . " -> " . $antragData['status'];
-                }
-                if ($antrag->bearbeitungsfrist != $antragData['bearbeitungsfrist']) {
-                    $antrag->notiz = trim($antrag->notiz) . "\n";
-                    $antrag->notiz .= date("d.m.Y.") . ": Bearbeitungsfrist: " .
-                                      $antrag->bearbeitungsfrist . " -> " . $antragData['bearbeitungsfrist'];
-                }
-                if ($antrag->fristverlaengerung != $antragData['fristverlaengerung']) {
-                    $antrag->notiz = trim($antrag->notiz) . "\n";
-                    $antrag->notiz .= date("d.m.Y.") . ": Fristverlängerung: " .
-                                      $antrag->fristverlaengerung . " -> " . $antragData['fristverlaengerung'];
-                }
-                if ($antrag->gestellt_am != $antragData['gestellt_am']) {
-                    $antrag->notiz = trim($antrag->notiz) . "\n";
-                    $antrag->notiz .= date("d.m.Y.") . ": Gestellt am: " .
-                                      $antrag->gestellt_am . " -> " . $antragData['gestellt_am'];
-                }
-            }
-            $antrag->titel              = $antragData['betreff'];
-            $antrag->typ                = $antragData['typ'];
-            $antrag->antrags_nr         = $antragData['antrags_nr'];
-            $antrag->gestellt_am        = $antragData['gestellt_am'];
-            $antrag->bearbeitungsfrist  = $antragData['bearbeitungsfrist'];
-            $antrag->fristverlaengerung = $antragData['fristverlaengerung'];
-            $antrag->status             = $antragData['status'];
-            $antrag->notiz              = '';
-            $antrag->save();
+            $this->importDatarow($antragData);
+        }
+    }
 
-            foreach ($antragData['stadtraetInnen'] as $stadtraetInData) {
-                $stadtraetin = Stadtraetin::findOne(['ris_id' => $stadtraetInData['id']]);
-                if ( ! $stadtraetin) {
-                    $stadtraetin         = new Stadtraetin();
-                    $stadtraetin->ris_id = $stadtraetInData['id'];
-                    $stadtraetin->name   = $stadtraetInData['name'];
-                    $stadtraetin->save();
-                }
-                $antrag->link('stadtraetinnen', $stadtraetin);
-            }
+    /**
+     * Daten von München Transparent importieren
+     */
+    public function actionImportFromMt()
+    {
+        $importUrl = \Yii::$app->params['importUrl'];
+        try {
+            $data = $this->downloadUrl($importUrl);
+        } catch (\Exception $e) {
+            $this->stderr($e->getMessage() . "\n\n");
+            return -1;
+        }
+        $data = json_decode($data, true);
+        foreach ($data as $antragData) {
+            $this->importDatarow($antragData);
         }
     }
 }
--- config/params.sample.php
+++ config/params.sample.php
@@ -3,5 +3,6 @@
 define('COOKIE_VALIDATION_KEY', '......');
 
 return [
+    'importUrl'  => 'https://www.muenchen-transparent.de/export/fraktionantraege/...',
     'adminEmail' => 'admin@example.com',
 ];
